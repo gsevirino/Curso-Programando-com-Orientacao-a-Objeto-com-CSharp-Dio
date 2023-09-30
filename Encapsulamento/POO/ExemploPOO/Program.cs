ee31affb9cccc03c472f821f0038f5c2b36d6e43
Gislaine <gislaine.sevirino@gmail.com>
2023-09-30 10:47:49 -0300

    encapsulamento

---
+++ Encapsulamento/POO/ExemploPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+            r.DefinirMedidas(30, 30);
+
+            System.Console.WriteLine($"Área: {r.ObterArea()}");
+        }
+    }
+}
