e2d9176ae99c8c0f36bb3cce9f02693807697b3c
Gislaine <gislaine.sevirino@gmail.com>
2023-09-30 10:07:21 -0300

    POO Abstração

---
+++ Abstracao/POO/ExemploPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.nome = "Bob";
+            p1.idade = 20;
+
+            p1.Apresentar();
+        }
+    }
+}
