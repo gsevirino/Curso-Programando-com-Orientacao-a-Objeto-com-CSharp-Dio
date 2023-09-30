2a6bd34995b203b3a80b61a5878dfefe7ac27c7a
Gislaine <gislaine.sevirino@gmail.com>
2023-09-30 13:16:55 -0300

    aulas de Abstração, Encapsulamento, Herança e Polimorfismo

---
+++ Polimorfismo/POO/tempoDeCompilacao/ExemploPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+            System.Console.WriteLine("Resultado da primeira soma " + calc.Somar(10, 10));
+            System.Console.WriteLine("Resultado da segunda soma " + calc.Somar(10, 10, 10));
+        }
+    }
+}
---
+++ Polimorfismo/POO/tempoDeExecucao/ExemploPOO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno p1 = new Aluno();
+            p1.Nome = "Bob";
+            p1.Idade = 30;
+            p1.Documento = "25698845687";
+            p1.Nota = 10;
+            p1.Apresentar();
+
+            Professor p2 = new Professor();
+            p2.Nome = "Bob";
+            p2.Idade = 30;
+            p2.Documento = "25698845687";
+            p2.Salario = 3000;
+            p2.Apresentar();
+
+        }
+    }
+}
